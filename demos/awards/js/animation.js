e69039a3189e2ebf103e37d4ec876af065d8dca9
jlatonio <jlatonio@adobe.com>
2019-01-04 15:58:26 -0800

    css

--- demos/awards/js/animation.js
+++ demos/awards/js/animation.js
@@ -1,198 +1,235 @@
 // toggles drop down and toggle arrow icon
 $(".custom-dropdown-group,.custom-dropdown-close").hide();
 $(".custom-dropdown-open").on("click", function() {
-    // closes other elements other than this when clicked
-    'use strict';
-    $(".custom-dropdown-input").find(".custom-dropdown-close").not(this).trigger("click");
-    $("body").trigger("click");
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").removeClass("custom-dropdown-arrow-open");
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").addClass("custom-dropdown-arrow-close");
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").slideDown(300);
-    $(this).hide();
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-close").show();
-    return false;
+ // closes other elements other than this when clicked
+ 'use strict';
+ $(".custom-dropdown-input").find(".custom-dropdown-close").not(this).trigger("click");
+ $("body").trigger("click");
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").removeClass("custom-dropdown-arrow-open");
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").addClass("custom-dropdown-arrow-close");
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").slideDown(300);
+ $(this).hide();
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-close").show();
+ return false;
 });
 
 // closes menu when an item is selected
 $(".custom-dropdown-close").on("click", function() {
-    'use strict';
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").removeClass("custom-dropdown-arrow-close");
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").addClass("custom-dropdown-arrow-open");
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").slideUp(100);
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-open").show();
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-close").hide();
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-close").hide();
-    return false;
+ 'use strict';
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").removeClass("custom-dropdown-arrow-close");
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").addClass("custom-dropdown-arrow-open");
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").slideUp(100);
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-open").show();
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-close").hide();
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-close").hide();
+ return false;
 });
 
 // resets search when user clicks close
 $(".custom-select-close").hide();
 $("input[type='text']").closest(".custom-dropdown-outer").find(".custom-dropdown").on("click", function() {
-    'use strict';
-    $(this).closest(".custom-dropdown-outer").find(".custom-select-close").show();
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").hide();
-    return false;
+ 'use strict';
+ $(this).closest(".custom-dropdown-outer").find(".custom-select-close").show();
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").hide();
+ return false;
 });
 $("input[type='text']").closest(".custom-dropdown-outer").find(".custom-select-close").on("click", function() {
-    'use strict';
-    $(this).hide();
-    $(this).closest(".custom-dropdown-outer").find("input[type='text']").val("").focus();
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").hide();
-    $(this).closest(".custom-dropdown-outer").find(".custom-select-date").show();
-    return false;
+ 'use strict';
+ $(this).hide();
+ $(".modal-select").hide();
+ $(this).closest(".custom-dropdown-outer").find("input[type='text']").val("").focus().css({
+  "background": ""
+ });
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").hide();
+ $(this).closest(".custom-dropdown-outer").find(".custom-select-date").show();
+ $(this).closest(".custom-dropdown-outer").find(".custom-select-search").show();
+ return false;
 });
 
 // closes menu when user clicks away
 $("body").on("click", function() {
-    'use strict';
-    if ($(this).not(".custom-dropdown-outer")) {
-        $(".custom-dropdown-group").slideUp(100);
-        $(".custom-dropdown-open").show();
-        $(".custom-dropdown-close").hide();
-        $(".custom-dropdown-arrow-toggle").removeClass("custom-dropdown-arrow-close");
-        $(".custom-dropdown-arrow-toggle").addClass("custom-dropdown-arrow-open");
-    }
+ 'use strict';
+ if ($(this).not(".custom-dropdown-outer")) {
+  $(".custom-dropdown-group").slideUp(100);
+  $(".custom-dropdown-open").show();
+  $(".custom-dropdown-close").hide();
+  $(".custom-dropdown-arrow-toggle").removeClass("custom-dropdown-arrow-close");
+  $(".custom-dropdown-arrow-toggle").addClass("custom-dropdown-arrow-open");
+ }
+});
+$("input[type='text']").on("click", function() {
+ $(".custom-dropdown-close").trigger("click");
+ return false;
 });
 
 // adds the html to the input value for each button clicked
 $(".custom-dropdown").on("click", function() {
-    'use strict';
-    var option = $(this).text();
-    var val = $(this).attr("value");
-    var checkinput = $(this).closest(".custom-dropdown-outer").find("input[type='text']")
-    $(this).closest(".custom-dropdown-outer").find(checkinput).val(option);
-    $(checkinput).attr("value", val);
+ 'use strict';
+ var option = $(this).text();
+ var val = $(this).attr("value");
+ var checkinput = $(this).closest(".custom-dropdown-outer").find("input[type='text']")
+ $(this).closest(".custom-dropdown-outer").find(checkinput).val(option);
+ $(checkinput).attr("value", val);
 
-    // changes the form"s action link on specific choices
-    if ($("input[type='text']").attr("value") === "Thank You") {
-        $(".custom-dropdown-input").removeAttr("action");
-        $(".custom-dropdown-input").attr("action", "ty-entry.html");
-    } else {
-        $(".custom-dropdown-input").removeAttr("action");
-        $(".custom-dropdown-input").attr("action", "awards-entry.html");
-    }
+ // changes the form"s action link on specific choices
+ if ($("input[type='text']").attr("value") === "Thank You") {
+  $(".custom-dropdown-input").removeAttr("action");
+  $(".custom-dropdown-input").attr("action", "ty-entry.html");
+ } else {
+  $(".custom-dropdown-input").removeAttr("action");
+  $(".custom-dropdown-input").attr("action", "awards-entry.html");
+ }
 
-    // sets active state
-    $(".custom-dropdown").css("background-color", "");
-    'use strict';
-    $(this).css("background-color", "#e3e3e3");
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").removeClass("custom-dropdown-arrow-close");
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").addClass("custom-dropdown-arrow-open");
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").slideUp(100);
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-open").show();
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-close").hide();
-    return false;
+ // sets active state
+ $(".custom-dropdown").css("background-color", "");
+ 'use strict';
+ $(this).css("background-color", "#e3e3e3");
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").removeClass("custom-dropdown-arrow-close");
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-arrow-toggle").addClass("custom-dropdown-arrow-open");
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").slideUp(100);
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-open").show();
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-close").hide();
+ return false;
 });
 
 // hover state
 $(".custom-dropdown-outer").find(".custom-dropdown").on("mouseover click", function() {
-    'use strict';
-    $(this).closest(".custom-dropdown-outer").find(this).css({
-        "background-color": "",
-        "color": ""
-    });
-    $(this).closest(".custom-dropdown-outer").find(this).css({
-        "background-color": "#7593bf",
-        "color": "#fff"
-    });
-    return false;
+ 'use strict';
+ $(this).closest(".custom-dropdown-outer").find(this).css({
+  "background-color": "",
+  "color": ""
+ });
+ $(this).closest(".custom-dropdown-outer").find(this).css({
+  "background-color": "#7593bf",
+  "color": "#fff"
+ });
+ return false;
 });
 
 $(".custom-dropdown-outer").find(".custom-dropdown").on("mouseout", function() {
-    'use strict';
-    $(this).closest(".custom-dropdown-outer").find(this).css({
-        "background-color": "",
-        "color": ""
-    });
-    return false;
+ 'use strict';
+ $(this).closest(".custom-dropdown-outer").find(this).css({
+  "background-color": "",
+  "color": ""
+ });
+ return false;
 });
 
 // enables the submit button if fields are filled. adds focus to empty input area
 $(".store-continue").click(function(e) {
-    var isValid = true;
-    $("input[type='text']").each(function() {
-        if ($.trim($(this).val()) == "") {
-            isValid = false;
-            $(this).css({
-                "background": "#fff2f2"
-            });
-            $(".modal-alert").show();
-        } else {
-            $(this).css({
-                "background": ""
-            });
-        }
-    });
-    if (isValid == false)
-        e.preventDefault();
-    else
-        $("form").submit();
+ var isValid = true;
+ $("input[type='text']").each(function() {
+  if ($.trim($(this).val()) == "") {
+   isValid = false;
+   $(this).css({
+    "background": "#fff2f2"
+   });
+   $(".modal-alert").show();
+  } else {
+   $(this).css({
+    "background": ""
+   });
+  }
+ });
+ if (isValid == false)
+  e.preventDefault();
+ else
+  $("form").submit();
 });
 
 // modal alerts
 $(".modal-close").on("click", function() {
-    $(".modal-alert,.modal-confirmation,.modal-ineligible,.modal-share,.modal-select").hide();
-    $("textarea").focus();
-    $(".custom-dropdown-input").find("input:text[value='']:visible").first().focus();
-    return false;
-});
-
-$(".modal-close-ineligible").on("click", function() {
-    $(".custom-select").focus();
-    $(".custom-select").css({
-        "background": "#fff2f2"
-    });
-    return false;
-});
-
-$(".modal-close-select").on("click", function() {
-    $(".custom-select").focus();
-    $(".custom-select").css({
-        "background": "#fff2f2"
-    });
-    return false;
+ $(".modal-alert,.modal-confirmation,.modal-ineligible,.modal-share,.modal-select").hide();
+ $("textarea").focus();
+ $(".custom-dropdown-input").find("input:text[value='']:visible").first().focus();
+ return false;
 });
 
 // resets required fields to a white bg
 $(".custom-dropdown-scroll div").on("click", function() {
-    $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-menu").css({
-        "background": ""
-    });
-    return false;
+ $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-menu").css({
+  "background": ""
+ });
+ return false;
 });
 $("input[type='text'],textarea").keypress(function() {
-    $(this).css({
-        "background": ""
-    });
+ $(this).css({
+  "background": ""
+ });
 });
 
 // autofill email
 $(document).ready(function() {
-    $("input[type='text']").on("keyup change", function() {
-        var value = $(this).val().toLowerCase();
-        var option = $(this).val();
-        var item = option
-        if (option.length === 0) {
-            $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").hide();
-            $(this).closest(".custom-dropdown-outer").find(".custom-select-close").hide();
-            $(this).closest(".custom-dropdown-outer").find(".custom-select-date").show();
-        }
-        if (option.length > 1) {
-            $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").show();
-            $(this).closest(".custom-dropdown-outer").find(".custom-select-close").show();
-            $(this).closest(".custom-dropdown-outer").find(".custom-select-date").hide();
-        }
-        $(this).closest(".custom-dropdown-outer").find(".custom-dropdown").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-        });
-    });
-    return false;
+ $("input[type='text']").on("keyup change", function() {
+  var value = $(this).val().toLowerCase();
+  var option = $(this).val();
+  var item = option
+  if (option.length === 0) {
+   $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").hide();
+   $(this).closest(".custom-dropdown-outer").find(".custom-select-close").hide();
+   $(this).closest(".custom-dropdown-outer").find(".custom-select-date").show();
+   $(this).closest(".custom-dropdown-outer").find(".custom-select-search").show();
+  }
+  if (option.length > 0) {
+   $(this).closest(".custom-dropdown-outer").find(".custom-dropdown-group").show();
+   $(this).closest(".custom-dropdown-outer").find(".custom-select-close").show();
+   $(this).closest(".custom-dropdown-outer").find(".custom-select-date").hide();
+   $(this).closest(".custom-dropdown-outer").find(".custom-select-search").hide();
+  }
+  $(this).closest(".custom-dropdown-outer").find(".custom-dropdown").filter(function() {
+   $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+  });
+ });
+ return false;
 });
 
 // dropdown menu will block all keystrokes
 $(".custom-dropdown-menu").on("keyup", function() {
-    $(this).val("");
+ $(this).val("");
+ return false;
 });
+
+// enables the datepicker
+$(".custom-select-date, .custom-date").on("click", function() {
+ $(".custom-date").datepicker();
+ $(this).closest(".custom-dropdown-outer").find(".custom-date").datepicker("show");
+ return false;
+});
+
+// adds focus when the search icon is clicked
+$(".custom-select-search").on("click", function() {
+ $(this).closest(".custom-dropdown-outer").find("input[type='text']").focus();
+ return false;
+});
+
+// enables dropdown search
+$(function() {
+ $(".custom-search").autocomplete({
+  source: [
+   "Joseph Latonio",
+   "Rebecca Warner",
+   "Sri Krishnamoorthy"
+  ],
+  minLength: 1,
+  response: function(event, ui) {
+   // clears field if name is misspelled or invalid
+   if (ui.content.length === 0) {
+    $(this).val("");
+    return false;
+   } else {}
+  }
+ });
+});
+
+// clears field if there is only one character submitted
+// $("input[type='text']").on("change", function() {
+//         if($(this).val().length == 0) {
+//         $('.custom-select').closest(".custom-dropdown-outer").find(".custom-dropdown-group").hide();
+//         $(this).val("");
+//         return false;
+//     } 
+// });
+
 // Validates Email ID. Not needed for now since the user only can only select what is available via dropdown
 // $('.custom-select').on("change", function() {
 //     var txt = $('.custom-select').val();
@@ -206,8 +243,3 @@ $(".custom-dropdown-menu").on("keyup", function() {
 //         $('.modal-ineligible').show();
 //     }
 // });
-
-$(".custom-select-date, .custom-date").on("click", function() {
-    $( ".custom-date" ).datepicker();
-    $(this).closest(".custom-dropdown-outer").find( ".custom-date" ).datepicker("show");
-});
--- demos/awards/js/localstorage.js
+++ demos/awards/js/localstorage.js
@@ -1,199 +1,168 @@
 // loads latest every session for the ty page
 $(".store-data-ty").each(function() {
-    'use strict';
-    var text = "store-data-ty"
-    var value = localStorage.getItem(text);
-    $(this).val(value);
-    // If textarea is empty, a new class is added        
-    if ($.trim($(this).val()) === "") {
-        $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-active").addClass("notes-btn-each-empty");
-    } else {
-        $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-empty").addClass("notes-btn-each-active");
-    }
-    $(this).attr("text");
+ 'use strict';
+ var text = "store-data-ty"
+ var value = localStorage.getItem(text);
+ $(this).val(value);
+ // If textarea is empty, a new class is added        
+ if ($.trim($(this).val()) === "") {
+  $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-active").addClass("notes-btn-each-empty");
+ } else {
+  $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-empty").addClass("notes-btn-each-active");
+ }
+ $(this).attr("text");
 });
 
 // loads latest every session for the awards page
 $(".store-data-awards").each(function() {
-    'use strict';
-    var text = "store-data-awards"
-    var value = localStorage.getItem(text);
-    $(this).val(value);
-    // If textarea is empty, a new class is added        
-    if ($.trim($(this).val()) === "") {
-        $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-active").addClass("notes-btn-each-empty");
-    } else {
-        $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-empty").addClass("notes-btn-each-active");
-    }
-    $(this).attr("text");
+ 'use strict';
+ var text = "store-data-awards"
+ var value = localStorage.getItem(text);
+ $(this).val(value);
+ // If textarea is empty, a new class is added        
+ if ($.trim($(this).val()) === "") {
+  $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-active").addClass("notes-btn-each-empty");
+ } else {
+  $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-empty").addClass("notes-btn-each-active");
+ }
+ $(this).attr("text");
 });
 // save icon. hidden by default
 $(".notes-btn-each").hide();
 
 // saves data on click
 $(".store-save").on("click", function() {
-    'use strict';
-    $(this).closest("form").find(".store-data-ty").css({
-        "background": ""
-    });
-    $(this).closest(".notes-container").find(".store-data-ty").each(function() {
-        var text = "store-data-ty"
-        var value = $(this).val();
-        localStorage.setItem(text, value);
-        var value = localStorage.getItem(text);
-        $(this).val(value);
-    });
-    
-    $(this).closest(".notes-container").find(".store-data-awards").each(function() {
-        var text = "store-data-awards"
-        var value = $(this).val();
-        localStorage.setItem(text, value);
-        var value = localStorage.getItem(text);
-        $(this).val(value);
-    });
+ 'use strict';
+ $(this).closest("form").find(".store-data-ty").css({
+  "background": ""
+ });
+ $(this).closest(".notes-container").find(".store-data-ty").each(function() {
+  var text = "store-data-ty"
+  var value = $(this).val();
+  localStorage.setItem(text, value);
+  var value = localStorage.getItem(text);
+  $(this).val(value);
+ });
 
-    // save animation
-    $(".notes-btn-each").show();
-    'use strict';
-    $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-empty").addClass("notes-btn-each-active");
-    setTimeout(function() {
-        $(".notes-btn-each").fadeOut();
-    }, 1000);
-    return false;
+ $(this).closest(".notes-container").find(".store-data-awards").each(function() {
+  var text = "store-data-awards"
+  var value = $(this).val();
+  localStorage.setItem(text, value);
+  var value = localStorage.getItem(text);
+  $(this).val(value);
+ });
+
+ // save animation
+ $(".notes-btn-each").show();
+ 'use strict';
+ $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-empty").addClass("notes-btn-each-active");
+ setTimeout(function() {
+  $(".notes-btn-each").fadeOut();
+ }, 1000);
+ return false;
 });
 
 // resets each field 
 $(".store-reset").on("click", function() {
-    'use strict';
-    $(".custom-select-close").hide();
-    $(this).closest("form").removeAttr("action");
-    $(this).closest("form").find("input[type=text]").val("").css({
-        "background": ""
-    });
-    $(this).closest("form").find("textarea").val("").css({
-        "background": ""
-    });
-    $(this).closest(".notes-container").find("textarea").val("");
-    $(this).closest(".notes-container").find(".store-data-ty").each(function() {
-        var text = "store-data-ty"
-        var value = $(this).val();
-        localStorage.setItem(text, value);
-        var value = localStorage.getItem(text);
-        $(this).val(value);
-    });
-    
-    $(this).closest(".notes-container").find(".store-data-awards").each(function() {
-        var text = "store-data-awards"
-        var value = $(this).val();
-        localStorage.setItem(text, value);
-        var value = localStorage.getItem(text);
-        $(this).val(value);
-    });
-    $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-active").addClass("notes-btn-each-empty");
+ 'use strict';
+ $(".custom-select-close").hide();
+ $(this).closest("form").removeAttr("action");
+ $(this).closest("form").find("input[type=text]").val("").css({
+  "background": ""
+ });
+ $(this).closest("form").find("textarea").val("").css({
+  "background": ""
+ });
+ $(this).closest(".notes-container").find("textarea").val("");
+ $(this).closest(".notes-container").find(".store-data-ty").each(function() {
+  var text = "store-data-ty"
+  localStorage.removeItem(text);
+ });
+
+ $(this).closest(".notes-container").find(".store-data-awards").each(function() {
+  var text = "store-data-awards"
+  localStorage.removeItem(text);
+ });
+ $(this).closest("form").find(".notes-btn-each").removeClass("notes-btn-each-active").addClass("notes-btn-each-empty");
 });
 
 // sends nomination. alerts if input is blank. adds focus to empty input area
 $(".store-nominate").click(function() {
-    $(".store-data-ty").each(function() {
-        'use strict';
-        var option = $(".store-data-ty").val();
-        if (option.length === 0) {
-            $(this).css({
-                "background": "#fff2f2"
-            });
-            $(".modal-alert").show();
-        }
-        if (option.length > 0) {
-            $("form").removeAttr("action");
-            $("form").attr("action", "ty-confirmation.html");
-            $("form").submit();
-            $(this).css({
-                "background": ""
-            });
-        }
-    });
-    $(".store-data-awards").each(function() {
-        'use strict';
-        var option = $(".store-data-awards").val();
-        if (option.length === 0) {
-            $(this).css({
-                "background": "#fff2f2"
-            });
-            $(".modal-alert").show();
-        }
-        if (option.length > 0) {
-            $("form").removeAttr("action");
-            $("form").attr("action", "awards-confirmation.html");
-            $("form").submit();
-            $(this).css({
-                "background": ""
-            });
-        }
-    });
-    return false;
+ $(".store-data-ty").each(function() {
+  'use strict';
+  var option = $(".store-data-ty").val();
+  if (option.length === 0) {
+   $(this).css({
+    "background": "#fff2f2"
+   });
+   $(".modal-alert").show();
+  }
+  if (option.length > 0) {
+   $("form").removeAttr("action");
+   $("form").attr("action", "ty-confirmation.html");
+   $("form").submit();
+   $(this).css({
+    "background": ""
+   });
+  }
+ });
+ $(".store-data-awards").each(function() {
+  'use strict';
+  var option = $(".store-data-awards").val();
+  if (option.length === 0) {
+   $(this).css({
+    "background": "#fff2f2"
+   });
+   $(".modal-alert").show();
+  }
+  if (option.length > 0) {
+   $("form").removeAttr("action");
+   $("form").attr("action", "awards-confirmation.html");
+   $("form").submit();
+   $(this).css({
+    "background": ""
+   });
+  }
+ });
+ return false;
 });
 
 // sends draft to email. alerts if input is blank. adds focus to empty input area
 $(".store-share").click(function() {
-    $("textarea").each(function() {
-        'use strict';
-        var option = $("textarea").val();
-        if (option.length === 0) {
-            $(this).css({
-                "background": "#fff2f2"
-            });
-            $(".modal-share").show();
-        }
-        if (option.length > 0) {
-            var mailto = "mailto:nominate@adobe.com";
-            var inputs = $("textarea");
-            var bodyStr = "";
-            var nominee = $(".store-nominee-enter").text();
-            var subjectStr = "subject=Thank You " + nominee + "!";
-            var cc = "?" + "cc=nominate@adobe.com,";
-            inputs.each(function(index, value) {
-                bodyStr += value.value;
-            });
-            window.location = mailto + cc + "&" + subjectStr + "&body=" + encodeURIComponent(bodyStr) + "%0D%0A%0D%0A%0D%0A Sincerely,%0D%0A Adobe Nominator";
-        }
-    });
-    return false;
+ $("textarea").each(function() {
+  'use strict';
+  var option = $("textarea").val();
+  if (option.length === 0) {
+   $(this).css({
+    "background": "#fff2f2"
+   });
+   $(".modal-share").show();
+  }
+  if (option.length > 0) {
+   var mailto = "mailto:nominate@adobe.com";
+   var inputs = $("textarea");
+   var bodyStr = "";
+   var nominee = $(".store-nominee-enter").text();
+   var subjectStr = "subject=Thank You " + nominee + "!";
+   var cc = "?" + "cc=nominate@adobe.com,";
+   inputs.each(function(index, value) {
+    bodyStr += value.value;
+   });
+   window.location = mailto + cc + "&" + subjectStr + "&body=" + encodeURIComponent(bodyStr) + "%0D%0A%0D%0A%0D%0A Sincerely,%0D%0A Adobe Nominator";
+  }
+ });
+ return false;
 });
 
 // needed for confirmation page, to clear local storage session when complete
 $(".store-complete-container").hide();
 $(".store-complete").on("click", function() {
-    'use strict';
-    $(".store-data-ty").val("");
-    $(".store-data-ty").each(function() {
-        var text = "store-data-ty"
-        var value = $(this).val();
-        localStorage.setItem(text, value);
-    });
-    $(".store-data-ty").each(function() {
-        var text = "store-data-ty"
-        var value = localStorage.getItem(text);
-        $(this).val(value);
-    });
-    $(".store-data-awards").val("");
-    $(".store-data-awards").each(function() {
-        var text = "store-data-awards"
-        var value = $(this).val();
-        localStorage.setItem(text, value);
-    });
-    $(".store-data-awards").each(function() {
-        var text = "store-data-awards"
-        var value = localStorage.getItem(text);
-        $(this).val(value);
-    });
+ $(".store-data-ty").each(function() {
+  var text = "store-data-ty"
+  localStorage.removeItem(text);
+ });
+ $(".store-data-awards").each(function() {
+  var text = "store-data-awards"
+  localStorage.removeItem(text);
+ });
 });
-
-
-
-
-
-
-
-
-
-
