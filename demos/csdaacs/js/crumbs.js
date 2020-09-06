4787fe931b68aac48b0895bd78aef94bbd359c21
jlatonio <45909943+jlatonio@users.noreply.github.com>
2020-09-05 23:26:17 -0700

    css

--- demos/csdaacs/js/crumbs.js
+++ demos/csdaacs/js/crumbs.js
@@ -2,10 +2,13 @@ function sticky_relocate() {
   var window_top = $(window).scrollTop();
   var div_top = $('#sticky-anchor').offset().top;
   if (window_top > div_top) {
-    $('.sticky').addClass('stick');
-  } else {
-    $('.sticky').removeClass('stick');
+    $('.hero-crumbs').addClass('stick');
+    $('.hero-crumbs').removeClass('sticky');
+  } if (window_top < div_top) {
+    $('.hero-crumbs').addClass('sticky');
+    $('.hero-crumbs').removeClass('stick');
   }
+    return false;
 }
 
 $(function() {
