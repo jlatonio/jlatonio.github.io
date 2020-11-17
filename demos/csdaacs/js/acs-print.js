e7ba77f07b4f6de4ebccbd49ea2d90ab69e0aa51
jlatonio <45909943+jlatonio@users.noreply.github.com>
2020-11-16 22:56:12 -0800

    css

--- demos/csdaacs/js/acs-print.js
+++ demos/csdaacs/js/acs-print.js
@@ -1,19 +1,4 @@
 $("#view-print").click(function () {
-    var devicePixelRatio = window.devicePixelRatio || 1;
-    var dpi = document.getElementById("finddpi").offsetHeight * devicePixelRatio * .5;
-    var width = 550;
-    var widthtotal = width / dpi;
-    var widthheight = widthtotal + "in ";
-    var cssPagedMedia = (function () {
-        var style = document.createElement("style");
-        document.head.appendChild(style);
-        return function (rule) {
-            style.innerHTML = rule;
-        };
-    }());
-    cssPagedMedia.size = function (size) {
-        cssPagedMedia("@page {size: " + widthheight + "}");
-    };
-    cssPagedMedia.size("portrait");
+    "use strict";
     window.print();
 });
