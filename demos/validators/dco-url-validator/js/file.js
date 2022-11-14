b5b32dab38151b645bd13c70fc33025e0f76d6e2
jlatonio <45909943+jlatonio@users.noreply.github.com>
2022-11-14 11:01:34 -0800

    css

--- demos/validators/dco-url-validator/js/file.js
+++ demos/validators/dco-url-validator/js/file.js
@@ -28,6 +28,24 @@ $(".img-container-image").each(function() {
 	});
 });
 
+function sticky_relocate() {
+  var window_top = $(window).scrollTop();
+  var div_top = $('#sticky-anchor').offset().top;
+  if (window_top > div_top) {
+    $('.hero-crumbs').addClass('stick');
+    $('.hero-crumbs').removeClass('sticky');
+  } if (window_top < div_top) {
+    $('.hero-crumbs').addClass('sticky');
+    $('.hero-crumbs').removeClass('stick');
+  }
+    return false;
+}
+
+$(function() {
+  $(window).scroll(sticky_relocate);
+  sticky_relocate();
+});
+
 
 // Click on individual image to generate URL feature
 $(".img-container-image").on("click", function() {
