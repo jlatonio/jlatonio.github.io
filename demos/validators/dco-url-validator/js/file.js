7d02bf67d287ba14163a77825df3e86923d7f722
jlatonio <45909943+jlatonio@users.noreply.github.com>
2022-11-16 22:57:30 -0800

    css

--- demos/validators/dco-url-validator/js/file.js
+++ demos/validators/dco-url-validator/js/file.js
@@ -1,93 +1,104 @@
     $(".add-html-here").show();
     $(".broken-image-urls-container").hide();
+    $(".close-report").hide();
+    // adds HTML
+    $(".add-html-go").on("click", function() {
+    	"use strict";
+    	var find_html = $(".add-html-form").val();
+    	$(".add-html-here,.inspect-url").fadeIn("5000");
+    	$(".broken-image-urls-container").hide();
+    	$(".add-html-here").html(find_html);
 
-// adds HTML
-$(".add-html-go").on("click", function() {
-	"use strict";
-	var find_html = $(".add-html-form").val();
-    $(".add-html-here,.inspect-url").show();
-    $(".broken-image-urls-container").hide();
-	$(".add-html-here").html(find_html);
+    	// List all broken URLs in an ordered list
+    	$(".img-container-image").each(function() {
+    		"use strict";
 
-	// List all broken URLs in an ordered list
-	$(".img-container-image").each(function() {
-		"use strict";
-		$(this).error(function() {
-			$(this).closest(".img-container").addClass("img-container-error");
-			$(this).closest(".img-container").find("p");
-			var label_image = $(this).closest(".img-container").find(".image-label").text();
-			var broken_url = $(this).attr("src");
-			$('.broken-image-urls').text('Broken Image URL Report:');
-			$('.broken-image-urls').after('<li>' + label_image + ' - ' + broken_url);
-			$(".broken-image-urls-container li").sort(asc_sort).appendTo('.broken-image-urls-container');
+    		$(this).error(function() {
+    			$(this).closest(".img-container").addClass("img-container-error");
+    			$(this).closest(".img-container").find("p");
+    			var label_image = $(this).closest(".img-container").find(".image-label").text();
+    			var broken_url = $(this).attr("src");
+    			$('.broken-image-urls').text('Broken Image URL Report:');
+    			$('.broken-image-urls').after('<li>' + label_image + ' - ' + broken_url);
+    			$(".broken-image-urls-container li").sort(asc_sort).appendTo('.broken-image-urls-container');
 
-			// accending sort
-			function asc_sort(a, b) {
-				return ($(b).text()) < ($(a).text()) ? 1 : -1;
-			}
-			return false;
-		});
-	});
+    			// accending sort
+    			function asc_sort(a, b) {
+    				return ($(b).text()) < ($(a).text()) ? 1 : -1;
+    			}
+    			return false;
+    		});
+    	});
 
-	function sticky_relocate() {
-		var window_top = $(window).scrollTop();
-		var div_top = $('#sticky-anchor').offset().top;
-		if (window_top > div_top) {
-			$('.hero-crumbs').addClass('stick');
-			$('.hero-crumbs').removeClass('sticky');
-		}
-		if (window_top < div_top) {
-			$('.hero-crumbs').addClass('sticky');
-			$('.hero-crumbs').removeClass('stick');
-		}
-		return false;
-	}
+    	function sticky_relocate() {
+    		var window_top = $(window).scrollTop();
+    		var div_top = $('#sticky-anchor').offset().top;
+    		if (window_top > div_top) {
+    			$('.hero-crumbs').addClass('stick');
+    			$('.hero-crumbs').removeClass('sticky');
+    		}
+    		if (window_top < div_top) {
+    			$('.hero-crumbs').addClass('sticky');
+    			$('.hero-crumbs').removeClass('stick');
+    		}
+    		return false;
+    	}
 
-	$(function() {
-		$(window).scroll(sticky_relocate);
-		sticky_relocate();
-	});
+    	$(function() {
+    		$(window).scroll(sticky_relocate);
+    		sticky_relocate();
+    	});
 
-	$(function() {
-		$(".inspect-url-icon").on("click", function() {
-			"use strict";
-			$(".inspect-url").removeAttr("srcdoc");
-			var url_image = $(this).closest(".img-container").find(".img-container-image").attr('src');
-			$(".inspect-url").attr("srcdoc", url_image);
-			return false;
-		});
+    	$(function() {
+    		$(".inspect-url-icon").on("click", function() {
+    			"use strict";
+    			$(".inspect-url").removeAttr("srcdoc");
+    			var url_image = $(this).closest(".img-container").find(".img-container-image").attr('src');
+    			$(".inspect-url").attr("srcdoc", url_image);
+    			return false;
+    		});
 
-		// Click on individual image to generate URL feature
-		$(".inspect-preset-icon").on("click", function() {
-			"use strict";
-			$(".inspect-url").removeAttr("srcdoc");
-			var inspect_url = $(this).closest(".img-container").find(".img-container-image").attr("src");
-			var inspect_preset = inspect_url + "&req=resolve";
-			$(".inspect-url").attr("src", inspect_preset);
-			return false;
-		});
+    		// Click on individual image to generate URL feature
+    		$(".inspect-preset-icon").on("click", function() {
+    			"use strict";
+    			$(".inspect-url").removeAttr("srcdoc");
+    			var inspect_url = $(this).closest(".img-container").find(".img-container-image").attr("src");
+    			var inspect_preset = inspect_url + "&req=resolve";
+    			$(".inspect-url").attr("src", inspect_preset);
+    			return false;
+    		});
 
-		// Click on individual video to generate URL feature
-		$(".inspect-video-icon").on("click", function() {
-			"use strict";
-			$(".inspect-url").removeAttr("srcdoc");
-			var url_video = $(this).closest(".video-container").find(".video-container-image source").attr("src");
-			$(".inspect-url").attr("srcdoc", url_video);
-			return false;
-		});
-        
-        
-        $(".view-report").click(function () {
-            "use strict";
-            $(".add-html-here,.inspect-url").hide();
-            $(".broken-image-urls-container").show();
-            $("html, body").animate({
-                    scrollTop: $(".broken-image-urls-container").offset().top - 170
-                },
-                300, "easeInOutQuad");
-            return false;
-        });
-        
-	});
-	return false;
-});
+    		// Click on individual video to generate URL feature
+    		$(".inspect-video-icon").on("click", function() {
+    			"use strict";
+    			$(".inspect-url").removeAttr("srcdoc");
+    			var url_video = $(this).closest(".video-container").find(".video-container-image source").attr("src");
+    			$(".inspect-url").attr("srcdoc", url_video);
+    			return false;
+    		});
+
+    		$(".view-report").click(function() {
+    			"use strict";
+    			$(".close-report").show();
+    			$(".add-html-here,.inspect-url").hide();
+    			$(".broken-image-urls-container").show();
+
+    			$("html, body").animate({
+    					scrollTop: $(".broken-image-urls-container").offset().top - 170
+    				},
+    				300, "easeInOutQuad");
+    			return false;
+    		});
+
+    		$(".close-report").click(function() {
+    			"use strict";
+    			$(this).hide();
+    			$(".view-report").show();
+    			$(".add-html-here,.inspect-url").slideDown();
+    			$(".broken-image-urls-container").slideUp();
+    			return false;
+    		});
+
+    	});
+    	return false;
+    });
