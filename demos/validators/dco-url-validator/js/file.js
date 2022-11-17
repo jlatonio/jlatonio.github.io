b99ddbc75746d5dd12ca69e6cc5776bb2fda3be6
jlatonio <45909943+jlatonio@users.noreply.github.com>
2022-11-16 23:33:27 -0800

    css

--- demos/validators/dco-url-validator/js/file.js
+++ demos/validators/dco-url-validator/js/file.js
@@ -7,7 +7,7 @@
     	var find_html = $(".add-html-form").val();
     	$(".broken-image-urls-container").hide();
     	$(".add-html-here").html(find_html);
-    	$(".add-html-here,.inspect-url").fadeIn();
+    	$(".add-html-here,.inspect-url").fadeIn(1000,"swing");
         
     	// List all broken URLs in an ordered list
     	$(".img-container-image").each(function() {
